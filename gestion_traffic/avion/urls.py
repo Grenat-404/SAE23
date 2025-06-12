c6862bdbc589404bb348cfdf4d0a5f3038a05e5c
Grenat-404 <mattdej.pl@gmail.com>
2025-06-12 16:36:16 +0200

    Update

--- gestion_traffic/avion/urls.py
+++ gestion_traffic/avion/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('aeroport/<int:id>/', views.readAeroports, name='afficheAeroports'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
